c3b8773fd60d4f9037cfee0cbc72ef9505401b22
n3250sasha <e975sasha@gmail.com>
2022-06-28 13:57:43 +0300

    Comment STATICFILES_DIRS in settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,10 +121,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # закоментував, бо не підгружало css файл
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, "static"),
+#]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
