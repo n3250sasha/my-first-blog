2ec7869007a8ee64af48279cc2d2ca28f981cc63
n3250sasha <e975sasha@gmail.com>
2022-06-29 16:10:02 +0300

    comment STATICFILES_DIRS in settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -13,9 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
-import mimetypes
-mimetypes.add_type("text/css", ".css", True)
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -124,9 +121,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(STATIC_ROOT, 'static'),
-]
+#STATICFILES_DIRS = [
+#    os.path.join(STATIC_ROOT, 'static'),
+#]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
